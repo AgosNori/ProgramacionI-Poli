a407db1acf0abc97a42836451f2e789a42af2ea9
agos <agosnoriega2018@gmail.com>
2023-09-09 10:49:14 -0300

    resultado ejer 4

--- Tutorias/Ejercicios/Clase2.py
+++ Tutorias/Ejercicios/Clase2.py
@@ -1,6 +1,8 @@
 #Ejercicio 4
 # Entradas
-
+n1 = int (input('Ingrese el primer numero: '))
+n2 = int (input('Ingrese el segundo numero: '))
 #Procesos
-
+total = n1+n2
 #Salidas
+print('El resultado de la suma es: ',total)
