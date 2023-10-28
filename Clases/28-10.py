269f771826c052306772a29712b11707c7d0d338
agos <agosnoriega2018@gmail.com>
2023-10-28 11:05:57 -0300

    ejerciicos de repaso

---
+++ Clases/28-10.py
@@ -0,0 +1,41 @@
+#Repaso
+#Ejercicio 1:
+'''Imprime <Hola Mundo> si a es mayor a b '''
+
+
+#Ejercicio 2:
+''' Determinar si alguien es menor de edad o no. Pide al usuario la
+edad por pantalla e imprime por pantalla si puede acceder a nuestra
+fiesta nocturna de BigBayData.com'''
+
+
+#Ejercicio 3:
+'''Imaginemos que en nuestra tienda hay un carnet por puntos y que alguien
+debe pagar el precio final de compra. Si tiene menos de 100 puntos
+realizaremos un descuento del 10%. Si es mayor a 100 y menor a
+150 aplicamos un 12%. Si tiene 150 un 15% y sino, el resto de los
+casos de mas de 150 , un 20% ¡ Crea la variable puntos y juega con ella!
+'''
+
+
+#Ejercicio 4:
+'''Guardar una contraseña como password. Crea un sistema de seguridad
+donde el ordenador muestre un mensaje <Ordenar Bloqueado. contraseña
+incorrecta> si el usuario falla la contraseña. En caso contrario ,
+que muestre por pantalla <Bienvenid@>'''
+
+
+#Ejercicio 5:
+'''Partiendo de la tarifa anual (que puede cambiar), nos piden que debemos calcular el precio de la tarifa de nuestro polideportivo, sabiendo las siguientes condiciones:
+Criterio 1: Si es mayor de edad y esta trabajando -> Paga el 100%
+Criterio 2: Si es menor de edad y esta trabajando -> Paga el 95%
+Criterio 3: Si es mayor de edad y no esta trabajando -> Paga el 75%
+Criterio 4: Si es menor de edad y no esta trabajando -> Paga el 50%
+'''
+
+# Ejercicio 6:
+''' Imprimir los numeros del 1 al 10 '''
+
+#Ejercicio 7:
+''' Tenemos la pantalla del celular bloqueada. Partiendo de un pin secreto, intentaremos desbloquear la pantalla. Tenemos hasta tres intentos.
+Simula el proceso con Python. En caso de acceder, lanza al usuario <Login Correcto> , sino <Llamando a la policia>'''
