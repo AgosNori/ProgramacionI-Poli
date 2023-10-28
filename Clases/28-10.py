873ce26fc4fe2a32d411ee2b6e6c6611d0ba2752
agos <agosnoriega2018@gmail.com>
2023-10-28 12:20:19 -0300

    resultado del ejercicio 4

--- Clases/28-10.py
+++ Clases/28-10.py
@@ -1,7 +1,7 @@
 #Repaso
 #Ejercicio 1:
 '''Imprime <Hola Mundo> si a es mayor a b '''
-
+'''
 # entradas
 a = 36
 b = 65
@@ -11,38 +11,65 @@ if a > b :
     print('HOLA MUNDO')
 else:
     print('chau')
-
-
-
-
-
-
-
-
-
-
-
-
+'''
 #Ejercicio 2:
-''' Determinar si alguien es menor de edad o no. Pide al usuario la
-edad por pantalla e imprime por pantalla si puede acceder a nuestra
-fiesta nocturna de BigBayData.com'''
+''' Determinar si alguien es menor de edad o no.
+Pide al usuario la edad por pantalla e imprime por
+pantalla si puede acceder a nuestra fiesta nocturna de
+BigBayData.com'''
 
+'''
+#entradas
+edad = int(input('Porfavor ingrese su edad : '))
+#procesos y salidas
 
-#Ejercicio 3:
-'''Imaginemos que en nuestra tienda hay un carnet por puntos y que alguien
-debe pagar el precio final de compra. Si tiene menos de 100 puntos
-realizaremos un descuento del 10%. Si es mayor a 100 y menor a
-150 aplicamos un 12%. Si tiene 150 un 15% y sino, el resto de los
-casos de mas de 150 , un 20% ¡ Crea la variable puntos y juega con ella!
+if edad > 18:
+    print('Puede ingresar')
+else:
+    print('No puede ingresar')
+
 '''
 
+#Ejercicio 3:
+'''Imaginemos que en nuestra tienda hay un carnet por
+puntos y que alguien debe pagar el precio final de compra.
+Si tiene menos de 100 puntos realizaremos un descuento
+del 10%. Si es mayor a 100 y menor a 150 aplicamos un 12%.
+Si tiene 150 un 15% y sino, el resto de los casos de mas
+de 150 , un 20% ¡ Crea la variable puntos y juega con ella!
+'''
+'''
+
+# entradas
+carnetDescuentos = int(input('Porfavor ingrese sus puntos: '))
+precio = 35000
+if carnetDescuentos < 100:
+    print('Se aplica un descuento del 10%')
+    total = precio - (precio* 0.10)
+    print('El total a pagar es: ',total)
+elif carnetDescuentos  > 100 and carnetDescuentos < 150:
+    print('Se aplica un descuento del 12%')
+    total = precio - (precio* 0.12)
+    print('El total a pagar es: ',total)
+elif carnetDescuentos == 150:
+    print('Se aplica un descuento del 15%') 
+    total = precio - (precio* 0.15)
+    print('El total a pagar es: ',total)
+elif carnetDescuentos > 150:
+    print('Se aplica un descuento del 20%')
+    total = precio - (precio* 0.20)
+    print('El total a pagar es: ',total)
+'''
 
 #Ejercicio 4:
 '''Guardar una contraseña como password. Crea un sistema de seguridad
 donde el ordenador muestre un mensaje <Ordenar Bloqueado. contraseña
 incorrecta> si el usuario falla la contraseña. En caso contrario ,
 que muestre por pantalla <Bienvenid@>'''
+
+
+
+
 
 
 #Ejercicio 5:
